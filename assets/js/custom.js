a56d749c2b5a8d099f14ba2b3fea11c01a482406
Sharon <sharon.peishan.kennedy@gmail.com>
2017-05-31 17:38:56 -0400

    added carousel

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,23 +7,19 @@ PARTICIPANT_TYPES = [
   'science'
 ];
 
-function selectAgendaItem() {
-  $('.day-selector button, .agenda-item').removeClass('active');
-
-  var selectedDay = $(this).data('day');
-  var selectedDayClass = '.' + selectedDay;
-
-  $(selectedDayClass).addClass('active');
+function activateButton() {
+  $('.day-selector button').removeClass('active');
+  $(this).addClass('active');
 };
 
 function cycleParticipants() {
-  var textSpan = document.getElementById('cycle-participants');
-  var currentText = textSpan.textContent;
+  var textSpan = $('#cycle-participants');
+  var currentText = textSpan.text();
   var currentTextIndex = PARTICIPANT_TYPES.indexOf(currentText);
   var newPosition = currentTextIndex >= (PARTICIPANT_TYPES.length -1) ? 0 : currentTextIndex + 1
   var newText = PARTICIPANT_TYPES[newPosition];
 
-  textSpan.textContent = newText;
+  textSpan.text(newText);
 };
 
 function activateTrack() {
@@ -33,8 +29,7 @@ function activateTrack() {
 
 
 $(function(){
-  $('.day-selector button').on('click', selectAgendaItem);
+  $('.day-selector button').on('click', activateButton);
   $('.track').on('mouseover', activateTrack)
   window.setInterval(cycleParticipants, 2500);
-
 });
